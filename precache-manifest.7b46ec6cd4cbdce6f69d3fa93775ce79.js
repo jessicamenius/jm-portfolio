cde3978e033e0655b816b95a3fd11ebf8ecfeaa3
jessicamenius <jessicamenius@gmail.com>
2020-10-07 18:51:44 -0500

    Updates

---
+++ precache-manifest.7b46ec6cd4cbdce6f69d3fa93775ce79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b962eced123bd5f2c91cdb74bbd2db5",
+    "url": "/jm-portfolio/index.html"
+  },
+  {
+    "revision": "fa9c998a6e8278467fc6",
+    "url": "/jm-portfolio/static/css/main.d261db47.chunk.css"
+  },
+  {
+    "revision": "2319a38954aace246830",
+    "url": "/jm-portfolio/static/js/2.672a0d9a.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/jm-portfolio/static/js/2.672a0d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa9c998a6e8278467fc6",
+    "url": "/jm-portfolio/static/js/main.65dd12b6.chunk.js"
+  },
+  {
+    "revision": "03d9289d3d9f90ec5b2b",
+    "url": "/jm-portfolio/static/js/runtime-main.cdff8db1.js"
+  },
+  {
+    "revision": "40b5c0522304acc161f7db845dd66cd3",
+    "url": "/jm-portfolio/static/media/linkedin_logo.40b5c052.png"
+  }
+]);
